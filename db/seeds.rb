d944693f2d7eca073c2cb6f7264c964e5d2ed713
victor king <victor.king@essec.edu>
2018-11-08 12:37:32 +0800

    added devise & generated user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
+
 50.times do
   User.create!(
   email: Faker::Internet.email,
+  password: Faker::Internet.password,
   first_name: Faker::Name.first_name,
   last_name: Faker::Name.last_name,
   phone_number: Faker::PhoneNumber.phone_number,
