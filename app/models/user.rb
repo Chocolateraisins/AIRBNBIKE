f6f0ea752637ab1beebc47192c2d7e2680a79600
victor king <victor.king@essec.edu>
2018-11-08 16:17:40 +0800

    corrected minor syntax error in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
   has_many :bikes, dependent: :destroy
   has_many :bookings
 
-  validates :email, :first_name, :last_name, :phone_number, :presence true
+  validates :email, :first_name, :last_name, :phone_number, presence: true
   validates :email, :phone_number, uniqueness: true
 end
