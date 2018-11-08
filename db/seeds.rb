7380167022a5a82b91ad6e0d61848416b2ba30be
Jonas Dieterle <jonas.dieterle@gmail.com>
2018-11-08 12:33:59 +0800

    Create seeding method for Bike model (dependent on Users being seeded first)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Bike.destroy_all if Rails.env = "DEVELOPMENT"
+puts "creating 100 new bikes"
+100.times do |bike|
+  bike = Bike.new(
+    category: "Faker::Vehicle.drive_type" #for category
+    price: [5,10,15,25].sample #for price
+    brand: "Faker::Vehicle.make" #for brand
+    engine_size: [50,100,150,250,500].sample #for engine_size
+    user: User.all.sample
+  )
+  bike.save
+end
+puts "done creating 100 bikes"
