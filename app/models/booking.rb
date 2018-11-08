2a24b4d1dc7a56e0abae27baeda0106646bc479a
Jonas <43283863+codenas@users.noreply.github.com>
2018-11-08 15:04:47 +0800

    Update booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :bike
-
-  validates :bike, uniqueness: scope { scope: :user}
   validates :start_date, :end_date, presence: true
   validates :delivery_address, length: { minimum: 5 }
 end
