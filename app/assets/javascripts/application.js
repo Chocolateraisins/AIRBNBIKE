cb2a8808c4855a25a453c3e718e562d0c0a09a4e
victor king <victor.king@essec.edu>
2018-11-09 14:11:36 +0800

    made another small edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,8 +5,8 @@ var ml4 = {};
 ml4.opacityIn = [0,1];
 ml4.scaleIn = [0.2, 1];
 ml4.scaleOut = 3;
-ml4.durationIn = 800;
-ml4.durationOut = 600;
+ml4.durationIn = 950;
+ml4.durationOut = 850;
 ml4.delay = 500;
 
 anime.timeline({loop: true})
