5deba739044c47a6a7ca61dc5d7a0a6bc5cf2680
victor king <victor.king@essec.edu>
2018-11-12 15:22:24 +0800

    done

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -5,12 +5,15 @@ class BikesController < ApplicationController
   end
 
   def index
-    # if params.key? "filter"
-    #   @bikes = Bike.where(category: params[:filter])
-    # else
-    @bikes = Bike.all
-    # end
-
+    if params[:filter].present?
+      @bikes = Bike.where(category: params[:filter])
+    # elsif params[:filter] == "motorbike"
+    #   @bikes = Bike.where(category: "motorbike")
+  # elsif params[:squeeze].integer?
+  #    @bikes = Bike.where(price: params[:squeeze])
+    else
+      @bikes = Bike.all
+    end
   end
 
 
